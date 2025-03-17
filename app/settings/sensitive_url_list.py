48f2b84917693caa3040fe891990ddb13f2607a5
k.takeda <52458640+hitohorobe@users.noreply.github.com>
2025-03-17 20:38:54 +0900

    fix: センシティブ判定URLの追加

--- app/settings/sensitive_url_list.py
+++ app/settings/sensitive_url_list.py
@@ -2,6 +2,8 @@ PORN_URL_LIST = [
     "www.dlsite.com",
     "www.dmm.co.jp",
     "www.dmm.com",
+    "al.dmm.co.jp",
+    "al.dmm.com",
     "xvideos.com",
     "pornhub.com",
     "xhamster.com",
