c0dd49dce880d9e913cfd1e670d62f402568288d
k.takeda <52458640+hitohorobe@users.noreply.github.com>
2025-08-26 12:20:46 +0900

    Add new URLs to sensitive URL list
    ＜アフィリエイトリンクドメインの変更＞
    【変更前（現行ドメイン）】
    　https://al.dmm.co.jp
    
    【変更後（新ドメイン）】
    　https://al.fanza.co.jp
    
    ◇対象サービス
    FANZA動画
    
    ◇変更内容
    【変更前（旧URL）】
    　https://www.dmm.co.jp/digital/videoa/-/detail
    
    【変更後（新URL）】
    　https://video.dmm.co.jp/av/content/
    
    【変更点】
    ■アフィリエイトリンク
    www.dlsite.com → dlaf.jp
    
    ■シェアリンク
    www.dlsite.com → dlsharing.com

--- app/settings/sensitive_url_list.py
+++ app/settings/sensitive_url_list.py
@@ -1,10 +1,13 @@
 PORN_URL_LIST = [
     "www.dlsite.com",
     "dlaf.jp",
+    "dlsharing.com"
     "www.dmm.co.jp",
     "www.dmm.com",
     "al.dmm.co.jp",
     "al.dmm.com",
+    "al.fanza.com",
+    "video.dmm.com",
     "xvideos.com",
     "pornhub.com",
     "xhamster.com",
